f36a3634b39648ba443a6b1b033b374c4f261c4a
kimyega <kimyega@naver.com>
2026-01-06 17:50:55 +0900

    1

--- 08_Class_Method_Ex/src/ex06_Member/Contact.java
+++ 08_Class_Method_Ex/src/ex06_Member/Contact.java
@@ -2,14 +2,14 @@ package ex06_Member;
 
 public class Contact {
   private String homeTel;
-  private String moblie;
+  private String mobile;
   private Address address;
   
   public void setHomeTel(String homeTel) {
     this.homeTel = homeTel;
   }
   public void setMobile(String mobile) {
-    this.moblie = mobile;
+    this.mobile = mobile;
   }
   public void setAddress(Address address) {
     this.address = address;
@@ -18,8 +18,8 @@ public class Contact {
   public String getHomeTel() {
     return homeTel;
   }
-  public String getMoblie() {
-    return moblie;
+  public String getMobile() {
+    return mobile;
   }
   public Address getAddress() {
     return address;
