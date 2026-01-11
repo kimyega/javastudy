04081c3f0800b4d681ebc9cd2f0479604614fd43
kimyega <kimyega@naver.com>
2026-01-11 17:56:47 +0900

    1

--- 03_Control/src/ex02_loop/MainWrapper.java
+++ 03_Control/src/ex02_loop/MainWrapper.java
@@ -1,7 +1,7 @@
 package ex02_loop;
 
 public class MainWrapper {
-//  for 문
+  //  for 문
 //  1. 연속된 숫자(수열)를 생성하는 목적의 반복문이다.
 //  2. 형식
 //      for(초깃값; 조건문; 증감문) {
@@ -12,52 +12,53 @@ public class MainWrapper {
 //  
   public static void ex01() {
     // 1 ~ 10 출력하기
-    for(int n = 1; n <= 10; n++) {
+    for (int n = 1; n <= 10; n++) {
       System.out.println(n);
     }
     // 10 ~ 1 출력하기
-    for(int n = 10; n >= 1; n--) {
+    for (int n = 10; n >= 1; n--) {
       System.out.println(n);
     }
   }
+
   // 특정 횟수만큼 반복시키는 for문
   public static void ex02() {
     int count = 5;
-    for(int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++) {
       System.out.println(i + "번");
     }
   }
-  
-  
-  
+
+
   public static void ex99() {
-    for(int i = 1; i < 10; i++) {
+    for (int i = 1; i < 10; i++) {
       System.out.println("--- " + i + "단 ---");
-      for(int dan = 1; dan < 10; dan++) {
+      for (int dan = 1; dan < 10; dan++) {
         System.out.println(i + " * " + dan + " = " + i * dan);
-        
+
       }
       System.out.println();
     }
   }
+
   public static void expib() {
-  int inum = 5; 
-  int n1 = 0;
-  int n2 = 1;
-  int pnum = 0;
-  System.out.println("피보나치 숫자");
-  for(int n3 = 0; pnum < inum ; n3 = n1 + n2) {
-    
-    pnum += 1;
-    System.out.println(pnum + "번째 숫자 : " + n3 + " ");
-   n1 = n2;
-   n2 = n3;
+    int inum = 5;
+    int n1 = 0;
+    int n2 = 1;
+    int pnum = 1;
+    System.out.println("피보나치 숫자");
+    for (int n3 = n1 + n2; pnum < inum; n3 = n1 + n2) {
+
+      System.out.println(pnum + "번째 숫자 : " + n3 + " ");
+      pnum += 1;
+      n1 = n2;
+      n2 = n3;
+    }
+
+
   }
-    
-    
-  }
-  
-//  while 문
+
+  //  while 문
 //  1. 특정 조건을 만족하면 실행되는 반복문이다.
 //  2. 형식
 //    while(조건문) {
@@ -65,11 +66,12 @@ public class MainWrapper {
 //    }
   public static void ex03() {
     int n = 0;
-    while(n < 10) {
+    while (n < 10) {
       System.out.println(n);
       n++;
     }
   }
+
   /*
    * do while 문
    * 1. 반드시 1번은 실행되는 while문이다.
@@ -80,39 +82,36 @@ public class MainWrapper {
    */
   public static void ex04() {
     int n = 100;
-    do{
+    do {
       System.out.println(n);
       n++;
     }
-    while(n < 10); 
+    while (n < 10);
   }
-  
+
   /*
    * break 문
    * 1. switch문을 종료한다.
-   * 2. 반복문 (for, while, do while)을 종료한다. 
+   * 2. 반복문 (for, while, do while)을 종료한다.
    */
   public static void ex05() {
     // 무한루프 만드는 2가지 방법
     // 1. while(true) { } -- 추천
     // 2. for(;;) { }
-  // 무한루프와 break를 이용한 1 ~ 10 출력하기
+    // 무한루프와 break를 이용한 1 ~ 10 출력하기
     int n = 1;
-    while(true) {
-      if(n > 10) {
+    while (true) {
+      if (n > 10) {
         break;
       }
       System.out.println(n);
       n++;
-      
+
     }
-  
-  
-  
-  
-  
+
+
   }
-  
+
   /*
    * continue문
    * 1. 반복문의 실행 시작 지점부터 다시 실행한다.
@@ -120,34 +119,35 @@ public class MainWrapper {
    */
   public static void ex06() {
     // 1 ~ 50 사이 정수 중 7의 배수는 제외하고 출력하기
-    for(int i = 1; i <= 50; i++) {
-      if(i % 7 == 0) {
+    for (int i = 1; i <= 50; i++) {
+      if (i % 7 == 0) {
         continue;             // for문의 i++ 으로 돌아간다.
       }
       System.out.println(i);
     }
-    
+
   }
+
   public static void ex07() {
     // 중첩 for 문 
-   for(int day = 1; day <= 5; day++) {
-     System.out.println("--- "+ day + "일차 입니다.---");
-     for(int hour = 1; hour <= 8; hour++) {
-       System.out.println( "   "+ day + "일차" + hour + "교시입니다.");
-     }
-   }
+    for (int day = 1; day <= 5; day++) {
+      System.out.println("--- " + day + "일차 입니다.---");
+      for (int hour = 1; hour <= 8; hour++) {
+        System.out.println("   " + day + "일차" + hour + "교시입니다.");
+      }
+    }
   }
-  
+
   public static void main(String[] args) {
 //    ex01();
 //    ex02();
 //    ex99();
-    expib();
+//    expib();
 //    ex03();
 //    ex04();
 //    ex05();
 //    ex06();
-//    ex07();
-    
+    ex07();
+
   }
 }
